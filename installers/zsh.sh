092cdc5bd8b8dd0dd066e139144305e9fe6e05ab
Michele Longo <longomichele2015@gmail.com>
2024-09-27 23:49:33 +0200

    test | Minor fix

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -1,6 +1,6 @@
 echo "Installing zsh & ohmyzsh..."
 sudo pacman -S zsh --noconfirm
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
