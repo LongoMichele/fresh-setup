b5d6a8d3c29a9f91b93150e6bb2c023fcf6b1e2a
Michele Longo <longomichele2015@gmail.com>
2024-09-27 23:43:49 +0200

    test | Minor fixes

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -1,12 +1,12 @@
 echo "Installing zsh & ohmyzsh..."
 sudo pacman -S zsh --noconfirm
-zsh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
 newPlugins="plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting)"
-sed -i "s/^plugins=(=(.*)$)/$newPlugins" $HOME/.zsh
+sed -i "s/^plugins=(=(.*)$)/$newPlugins" $HOME/.zshrc
 
 cat ./dotfiles/.zshrc >> $HOME/.zshrc
 source $HOME/.zshrc
