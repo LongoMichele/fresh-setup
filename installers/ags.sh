0a23d5e5326822750ff09675463ea0d6a17ef3e3
Michele Longo <longomichele2015@gmail.com>
2024-09-28 20:50:21 +0200

    ags | Fixed hyprpicker && added dependencies

--- installers/ags.sh
+++ installers/ags.sh
@@ -1,7 +1,6 @@
 yay -S aylurs-gtk-shell-git
 yay -S matugen-bin
-yay -S hyprpicker-git
 curl -fsSL https://bun.sh/install | bash
-sudo pacman -S dart-sass fd brightnessctl swww fzf hyprpicker slurp wf-recorder swappy --noconfirm
-git clone https://github.com/Aylur/dotfiles.git
-cp -r dotfiles/ags $HOME/.config/ags
+sudo pacman -S dart-sass fd brightnessctl swww fzf hyprpicker slurp wf-recorder swappy libdbusmenu-gtk3 gnome-bluetooth-3.0 --noconfirm
+git clone https://github.com/Aylur/dotfiles.git aylurDotfiles
+cp -r aylurDotfiles/ags $HOME/.config/ags
