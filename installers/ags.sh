7ea9f5566445b2e763f2c58b9fe1addbb55d03dd
Michele Longo <longomichele2015@gmail.com>
2024-09-28 22:17:57 +0200

    ags | Bun update

--- installers/ags.sh
+++ installers/ags.sh
@@ -1,6 +1,6 @@
 yay -S aylurs-gtk-shell-git
 yay -S matugen-bin
-curl -fsSL https://bun.sh/install | bash
+curl -fsSL https://bun.sh/install | zsh
 sudo pacman -S dart-sass fd brightnessctl swww fzf hyprpicker slurp wf-recorder swappy libdbusmenu-gtk3 gnome-bluetooth-3.0 --noconfirm
 git clone https://github.com/Aylur/dotfiles.git aylurDotfiles
 cp -r aylurDotfiles/ags $HOME/.config/ags
--- installers/zsh.sh
+++ installers/zsh.sh
@@ -1,6 +1,7 @@
 echo "Installing zsh & ohmyzsh..."
 sudo pacman -S zsh --noconfirm
-zsh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s "$(which zsh)"
+RUNZSH=no sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
