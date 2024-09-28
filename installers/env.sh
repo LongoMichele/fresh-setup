d02f5021e1b732904143b7584961d5ce9be54296
Michele Longo <longomichele2015@gmail.com>
2024-09-28 19:13:54 +0200

    master | Added options

--- installers/env.sh
+++ installers/env.sh
@@ -1,6 +1,24 @@
 echo "Installing Wayland, Hyprland and relative tools..."
-sudo pacman -S wayland xorg-xwayland wlroots nvidia nvidia-utils hyprland hyprlock swaybg sddm kitty vim --noconfirm
-sudo systemctl enable sddm
+
+env="wayland xorg-xwayland wlroots hyprland kitty vim"
+nvidia="nvidia nvidia-utils"
+utils="hyprlock swaybg"
+login="sddm"
+
+install="$env $utils"
+read -p "Install Nvidia drivers? [Y/n]: " instNvidia
+if [[ $instNvidia == [yY] || $instNvidia == [yY][eE][sS] || ! $instNvidia ]]; then
+    install="$install $nvidia"
+fi
+read -p "Install SDDM? [Y/n]: " instLogin
+if [[ $instLogin == [yY] || $instLogin == [yY][eE][sS] || ! $instLogin ]]; then
+    install="$install $login"
+fi
+
+sudo pacman -S $install --noconfirm
+if [[ $instLogin == [yY] || $instLogin == [yY][eE][sS] || ! $instLogin ]]; then
+    sudo systemctl enable sddm
+fi
 
 pictures="$HOME/Pictures"
 if [ ! -d "$pictures" ]; then
